1eedae2e0aa26462970dfc1663c31f604536eeb7
elcomendante <karol@finsane.com>
2023-02-13 20:23:21 +0000

    helpers

--- src/miner.rs
+++ src/miner.rs
@@ -1,1 +1,22 @@
+use {
+    super::schema::miners,
+    crate::wallet::*,
+    crate::DBPooledConnection,
+    diesel::result::Error,
+    diesel::{ExpressionMethods, Insertable, QueryDsl, Queryable, RunQueryDsl},
+    rand::Rng,
+    serde::{Deserialize, Serialize},
+    uuid::Uuid,
+};
 
+// --------------- JSON Payload (REST)
+
+#[derive(Debug, Deserialize, Serialize)]
+pub struct Miner {
+    pub id: String,
+    pub address: String,
+    pub club_name: String,
+    pub nickname: String,
+    pub hash_rate: i32, // MH/s
+    pub shares_mined: i32,
+}
